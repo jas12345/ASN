9ea8af97a26d883196b2f8a747d2122351e650a4
Javier Raygoza Torres <javiconlinea@gmail.com>
2019-09-03 12:54:34 -0500

    fix de altura de grids en catalogos y cambio de posicion en los botones de cada grid

--- Proyecto/ASN/Scripts/PerfilEmpleados.js
+++ Proyecto/ASN/Scripts/PerfilEmpleados.js
@@ -1,4 +1,29 @@
-﻿var editando = 0;
+﻿function resizeGrid() {
+    var gridElement = $("#grid"),
+        dataArea = gridElement.find(".k-grid-content"),
+        gridHeight = gridElement.innerHeight(),
+        otherElements = gridElement.children().not(".k-grid-content"),
+        otherElementsHeight = 0;
+    otherElements.each(function () {
+        otherElementsHeight += $(this).outerHeight();
+    });
+    dataArea.height(gridHeight - otherElementsHeight);
+}
+
+function resizeWrapper() {
+    $("#parent").height(document.body.offsetHeight - 140);
+}
+
+$(window).resize(function () {
+    resizeWrapper();
+    resizeGrid();
+});
+
+$(document).ready(function () {
+    $(window).trigger("resize");
+});
+
+var editando = 0;
 
 function edit(e) {
     //var nombrePerfilEmpleados = $("#NombrePerfilEmpleados").data("kendoTextBox");
