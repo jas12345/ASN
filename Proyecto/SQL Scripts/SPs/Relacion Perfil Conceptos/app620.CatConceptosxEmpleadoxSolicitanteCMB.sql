74a95e46d44c03e7658204c44762bc5a4bdbb515
Alfredo Guerrero <guerrerolopez.8@nlsa.teleperformance.com>
2019-10-01 10:49:40 -0500

    Multiconceptos

---
+++ Proyecto/SQL Scripts/SPs/Relacion Perfil Conceptos/app620.CatConceptosxEmpleadoxSolicitanteCMB.sql
@@ -0,0 +1,68 @@
+ALTER PROCEDURE [app620].[CatConceptosxEmpleadoxSolicitanteCMB]
+(
+	  @Ident				INT = -1
+	 ,@Ident_Solicitante	INT = -1
+)
+AS
+
+BEGIN
+	SELECT
+			@Ident			= ISNULL(@Ident, -1)
+
+	SELECT 
+		 DISTINCT CC.ConceptoId As Ident
+		,CC.Descripcion As Valor
+
+		--CC.ClienteId, Perfil.Client_Ident,
+		--CC.ConceptoId, Perfil.ConceptoId,
+
+		--'Empleado' Empleado, Emp.*, 'Perfil' Perfil, Perfil.*, 'LocBIOS' LocBIOS, LocBIOS.*, 'Perfil Accesos' PerfilAccesos, PEA.*
+		--,'Conceptos' Conceptos, CC.*
+
+	FROM	
+		app620.CatEmployeeCCMSVw Emp
+	JOIN 
+		app620.CatPerfilEmpleados Perfil
+	ON	
+		(Emp.country_ident			= Perfil.Country_Ident			OR	Perfil.Country_Ident		= -1)
+	AND (Emp.Location_Ident			= Perfil.Location_Ident			OR	Perfil.Location_Ident		= -1)
+	AND (Emp.Client_Ident			= Perfil.Client_Ident			OR	Perfil.Client_Ident			= -1)
+	AND (Emp.Program_Ident			= Perfil.Program_Ident			OR	Perfil.Program_Ident		= -1)
+	AND (Emp.Contract_Type_Ident	= Perfil.Contract_Type_Ident	OR	Perfil.Contract_Type_Ident	= -1)
+	AND Emp.Ident = @Ident
+
+	JOIN [ASN5].[app620].[CatRelLocationBiosCCMSVw] LocBIOS
+	ON
+		LocBIOS.location_ccms = Emp.Location_Ident
+	AND
+		(LocBIOS.location_bios			= Perfil.City_Ident				OR	Perfil.City_Ident			= -1)
+
+	JOIN CatPerfilEmpleadosAccesos PEA							--Sección que conecta con los Perfiles asociados a permisos del solicitante
+	ON	
+		PEA.Perfil_Ident				= Perfil.Perfil_Ident
+	AND
+		PEA.EmpleadoId					= @Ident_Solicitante	-- Id Solicitante
+	AND 
+		Perfil.TipoAccesoId				= 1						--Tipo de acceso Solicitantes
+	AND
+		PEA.Active						= 1						-- Permiso activo para solicitante
+	JOIN
+		app620.RelPerfilEmpleadosConceptos REC
+	ON
+		REC.Perfil_Ident = Perfil.Perfil_Ident
+	JOIN
+		 app620.CatConceptos AS CC
+	--ON
+	--	(Emp.country_ident IN (SELECT Ident country_ident FROM fnSplit(CC.PaisId, ',')) OR CC.PaisId = -1)
+	ON
+		(CHARINDEX(CAST(Emp.country_ident as varchar(50)), CC.PaisId) > 0 OR CC.PaisId = '-1')
+	AND (Emp.[Client_Ident] = CC.ClienteId OR CC.ClienteId = -1)
+
+	AND (CC.ClienteId	= Perfil.Client_Ident	OR Perfil.Client_Ident	= -1	OR CC.ClienteId = -1)
+	AND (CC.ConceptoId	= REC.ConceptoId		OR REC.ConceptoId	IS NULL	OR CC.ConceptoId = -1)
+	AND CC.Active = 1
+END
+
+--SELECT * FROM CatPerfilEmpleados
+--SELECT * FROM CatConceptos
+
---
+++ Proyecto/SQL Scripts/SPs/Relacion Perfil Conceptos/app620.CatPerfilEmpleadosSel.sql
@@ -0,0 +1,70 @@
+ALTER PROC [app620].[CatPerfilEmpleadosSel]
+	@Perfil_Ident INT = 0 
+AS
+BEGIN
+
+	SET @Perfil_Ident = ISNULL(@Perfil_Ident, 0)
+
+	SELECT
+		 Perfil.[Perfil_Ident]
+		,[NombrePerfilEmpleados]
+		,Perfil.[Country_Ident]
+		,Pais.Country_Full_Name
+		,ISNULL(CCVW.Id, -1) City_Ident
+		,CCVW.Value City_Name
+		,Perfil.[Location_Ident]
+		,Location.full_name Location_Name
+		,Perfil.[Client_Ident]
+		,Client.Client_Name
+		,Perfil.[Program_Ident]
+		,Program.Program_Name
+		,Perfil.[Contract_Type_Ident]
+		,Contract.Contract_Type
+		,Perfil.ConceptoId
+		,Concepto.Descripcion Concepto
+		,Perfil.TipoAccesoId
+		,TipoAcceso.Descripcion TipoAcceso
+		,Perfil.Active
+		,CASE
+				WHEN COUNT(CC.Descripcion) > 1
+				THEN 'VARIOS'
+				ELSE (	SELECT	Descripcion
+						FROM	[app620].[RelPerfilEmpleadosConceptos] A WITH(NOLOCK)
+								JOIN app620.CatConceptos B ON A.ConceptoId = B.ConceptoId
+						WHERE	A.Perfil_Ident = Perfil.Perfil_Ident)
+		END AS ConceptoNombre
+	FROM [app620].[CatPerfilEmpleados] Perfil
+	JOIN [app620].[RelPerfilEmpleadosConceptos] REC WITH(NOLOCK) ON REC.Perfil_Ident = Perfil.Perfil_Ident
+	LEFT JOIN [app620].[CatCountryVw] Pais ON Pais.[Country_Ident] = Perfil.Country_Ident
+	LEFT JOIN [app620].[CatLocationVw] Location ON Location.Location_Ident = Perfil.Location_Ident
+	LEFT JOIN [app620].[CatClientVw] Client ON client.Client_Ident = Perfil.Client_Ident
+	LEFT JOIN [app620].[CatProgramVw] Program ON Program.Program_Ident = perfil.Program_Ident
+	LEFT JOIN [app620].[CatContractTypeVw] Contract ON Contract.Contract_Type_Ident = Perfil.Contract_Type_Ident
+	LEFT JOIN [app620].[CatConceptos] Concepto ON Concepto.ConceptoId = Perfil.ConceptoId
+	LEFT JOIN [app620].[CatTiposAcceso] TipoAcceso ON TipoAcceso.TipoAccesoId = Perfil.[TipoAccesoId]
+	LEFT JOIN [app620].[CatConceptos] CC ON CC.ConceptoId = REC.ConceptoId
+	LEFT JOIN [app620].[CatCityVw] CCVW ON CCVW.Id = Perfil.City_Ident
+	WHERE (Perfil.Perfil_Ident =@Perfil_Ident OR @Perfil_Ident = 0)
+	GROUP BY Perfil.[Perfil_Ident]
+			,[NombrePerfilEmpleados]
+			,Perfil.[Country_Ident]
+			,Pais.Country_Full_Name
+			,ISNULL(CCVW.Id, -1)
+			,CCVW.Value 
+			,Perfil.[Location_Ident]
+			,Location.full_name 
+			,Perfil.[Client_Ident]
+			,Client.Client_Name
+			,Perfil.[Program_Ident]
+			,Program.Program_Name
+			,Perfil.[Contract_Type_Ident]
+			,Contract.Contract_Type
+			,Perfil.ConceptoId
+			,Concepto.Descripcion
+			,Perfil.TipoAccesoId
+			,TipoAcceso.Descripcion
+			,Perfil.Active
+	ORDER BY Perfil.Perfil_ident
+END
+
+
