a77209de03b2280452e11a3effd74f141e192254
Jose Alfredo Saldana <jas1968@outlook.com>
2020-08-18 10:36:04 -0500

    Revert "first commit"
    This reverts commit de6596286d32a5bac50da45096b5f156db028fd3.

--- Proyecto/ASN/Models/Authentication.cs
+++ Proyecto/ASN/Models/Authentication.cs
@@ -21,10 +21,7 @@ namespace ASN.Models
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
             string json = "";
 
-            //json = httpClient.GetStringAsync(string.Empty).Result;
-            ///navarrogutierrez.6       666386
-            json = "[{\"Ccmspassword\":null,\"Ccmsusername\":null,\"Name\":\"Maydelline Elizabeth Navarro Gutierrez\",\"ident\":666386,\"manager_ident\":3345712,\"manager_first_name\":\"Jose\",\"manager_last_name\":\"Guereque Flores\",\"payroll_number\":\"599670\",\"current_status\":\"Active\",\"hire_date\":\"1996-09-02\",\"company_ident\":109,\"company_name\":\"Teleperformance MX NSC Monterrey\",\"location_ident\":317,\"location_name\":\"TPMXN Cuauhtemoc\",\"client_ident\":307,\"client_name\":\"Teleperformance Mexico\",\"program_ident\":49669,\"program_name\":\"TPMX CHTMC IT Development\",\"phone_ident\":\"599670\",\"account_id\":\"navarrogutierrez.6\",\"Position_Code_Title\":\"Payroll Coordinator\",\"Position_Code_Group\":\"\",\"Position_Code_Department\":\"Development Developer\",\"dob\":\"1971-04-23\",\"ETN_type\":\"IMSS\",\"ETN_flag\":\"28957102222\",\"sex\":\"Male\",\"email1\":\"luis.rosado@teleperformance.com\",\"civil_status\":\"Unknown\",\"phone_number\":\"\",\"pay_type\":\"\",\"benefit_schedule\":\"\",\"changes\":null,\"position_code_company_name\":null,\"position_code_full_name\":\"Director of Development\",\"position_code_abbr_name\":\"Director of Development\",\"Instalacion_Id\":2,\"employee_common_name\":\"Luis Enrique Rosado Baez\",\"manager_employee_common_name\":\"Jose Guereque Flores\",\"contract_type_full_name\":\"ICISA\",\"contract_type_ident\":547,\"city\":\"Monterrey\",\"state\":\"NLE\",\"position_code_type_ident\":109}]";
-
+            json = httpClient.GetStringAsync(string.Empty).Result;
 
             List<UserViewModel> Users = JsonConvert.DeserializeObject<List<UserViewModel>>(json);
 
